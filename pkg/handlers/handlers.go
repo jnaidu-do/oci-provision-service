db2673adf1e9be5648764c2f1ca7fd8e169c887d
jnaidu <jnaidu@digitalocean.com>
2025-06-12 16:31:34 +0530

    Oci -> oracle

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -174,6 +174,7 @@ func generateRandomSuffix() (string, error) {
 func (h *Handler) ProvisionBareMetal(w http.ResponseWriter, r *http.Request) {
 	// Add CORS headers
 	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+	w.Header().Set("Access-Control-Allow-Origin", "http://10.36.24.61:80/")
 	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
